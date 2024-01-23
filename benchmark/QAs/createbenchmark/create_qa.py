4f99a5ad5dd83545cc7ac3adbb335e8c646f92c6
cyrch <cyrine.chaabani19@gmail.com>
2024-01-23 16:05:21 +0100

    Add Benchmark code and samples

---
+++ benchmark/QAs/createbenchmark/create_qa.py
@@ -0,0 +1,240 @@
+import csv
+import random 
+import gurobipy as gp
+from gurobipy import GRB
+import numpy as np
+import subprocess
+import re
+from typing import Dict, List, Optional, Union
+
+from eventlet.timeout import Timeout
+
+VALUE_PROSUMER=[1,2,3,4]
+VALUE_TIMESTEP=[1,2,3]
+DATA_CODE_STR = "# OPTIGUIDE DATA CODE GOES HERE"
+CONSTRAINT_CODE_STR = "# OPTIGUIDE CONSTRAINT CODE GOES HERE"
+
+def VALUE_PERCENTAGE():
+    return random.choice([i for i in range(101)])
+
+def create_combinations(prosumer, timestep, percentage, num_combinations=20):
+    # Generate different combinations
+    combinations = []
+    if percentage:
+        for _ in range(num_combinations):
+            combination = {}
+
+            if prosumer:
+                combination['{{VALUE_PROSUMER}}'] = random.choice(VALUE_PROSUMER)-1
+
+            if timestep:
+                combination['{{VALUE_TIMESTEP}}'] = random.choice(VALUE_TIMESTEP)-1
+
+            
+            combination['{{VALUE_PERCENTAGE}}'] = VALUE_PERCENTAGE()
+            combinations.append(combination)
+    
+    elif prosumer and timestep:
+        for pro in VALUE_PROSUMER:
+            for t in VALUE_TIMESTEP:
+                combination = {}
+                combination['{{VALUE_PROSUMER}}'] = pro-1
+                combination['{{VALUE_TIMESTEP}}'] = t-1
+                combinations.append(combination)
+
+    elif prosumer:
+        for pro in VALUE_PROSUMER:
+            combination = {}
+            combination['{{VALUE_PROSUMER}}'] = pro-1
+            combinations.append(combination)
+
+    elif timestep:
+        for t in VALUE_TIMESTEP:
+            combination = {}
+            combination['{{VALUE_TIMESTEP}}'] = t-1
+            combinations.append(combination)
+
+
+    return combinations
+
+def _replace(src_code: str, old_code: str, new_code: str) -> str:
+    """
+    Inserts new code into the source code by replacing a specified old
+    code block.
+
+    Args:
+        src_code (str): The source code to modify.
+        old_code (str): The code block to be replaced.
+        new_code (str): The new code block to insert.
+
+    Returns:
+        str: The modified source code with the new code inserted.
+
+    Raises:
+        None
+
+    Example:
+        src_code = 'def hello_world():\n    print("Hello, world!")\n\n# Some
+        other code here'
+        old_code = 'print("Hello, world!")'
+        new_code = 'print("Bonjour, monde!")\nprint("Hola, mundo!")'
+        modified_code = _replace(src_code, old_code, new_code)
+        print(modified_code)
+        # Output:
+        # def hello_world():
+        #     print("Bonjour, monde!")
+        #     print("Hola, mundo!")
+        # Some other code here
+    """
+    pattern = r"( *){old_code}".format(old_code=old_code)
+    head_spaces = re.search(pattern, src_code, flags=re.DOTALL).group(1)
+    new_code = "\n".join([head_spaces + line for line in new_code.split("\n")])
+    rst = re.sub(pattern, new_code, src_code)
+    return rst
+
+
+def _insert_code(src_code: str, new_lines: str) -> str:
+    """insert a code patch into the source code.
+
+
+    Args:
+        src_code (str): the full source code
+        new_lines (str): The new code.
+
+    Returns:
+        str: the full source code after insertion (replacement).
+    """
+    if new_lines.find("addConstr") >= 0:
+        return _replace(src_code, CONSTRAINT_CODE_STR, new_lines)
+    else:
+        return _replace(src_code, DATA_CODE_STR, new_lines)
+    
+def _run_with_exec(src_code: str) -> Union[str, Exception]:
+    """Run the code snippet with exec.
+
+    Args:
+        src_code (str): The source code to run.
+
+    Returns:
+        object: The result of the code snippet.
+            If the code succeed, returns the objective value (float or string).
+            else, return the error (exception)
+    """
+    locals_dict = {}
+    locals_dict.update(globals())
+    locals_dict.update(locals())
+
+    timeout = Timeout(
+        60,
+        TimeoutError("This is a timeout exception, in case "
+                     "GPT's code falls into infinite loop."))
+    try:
+        exec(src_code, locals_dict, locals_dict)
+    except Exception as e:
+        return e
+    finally:
+        timeout.cancel()
+
+    try:
+        status = locals_dict["m"].Status
+        if status != GRB.OPTIMAL:
+            if status == GRB.UNBOUNDED:
+                ans = "unbounded"
+            elif status == GRB.INF_OR_UNBD:
+                ans = "inf_or_unbound"
+                m = locals_dict["m"]
+                m.computeIIS()
+                constrs = [c.ConstrName for c in m.getConstrs() if c.IISConstr]
+                ans += "\nConflicting Constraints:\n" + str(constrs)
+            elif status == GRB.INFEASIBLE:
+                ans = "infeasible"
+                m = locals_dict["m"]
+                m.computeIIS()
+                constrs = [c.ConstrName for c in m.getConstrs() if c.IISConstr]
+                ans += "\nConflicting Constraints:\n" + str(constrs)
+            else:
+                ans = "Model Status:" + str(status)
+        else:
+            ans = str(locals_dict["m"].objVal)
+    except Exception as e:
+        return e
+
+    return ans
+
+def compute_gt_questions(file_path, opt_script):
+    # Read the original Python script
+    with open(opt_script, 'r') as script_file:
+        original_script = script_file.read()
+
+    data_dict = {}
+    
+    with open(file_path, "r") as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        counter = 0
+        for row in csv_reader:
+            #print(row)
+            question = row['\ufeffQuestion']
+            gt_code = row['GT Code']
+            gt_obj_value = row['GT Objective Value']
+            sgp_code = row['SGP Code']
+            sgp_obj_value = row['SGP Objective Value']
+            
+            prosumer = "{{VALUE_PROSUMER}}" in question
+            timestep = "{{VALUE_TIMESTEP}}" in question
+            percentage = "{{VALUE_PERCENTAGE}}" in question
+            combinations_question = create_combinations(prosumer, timestep, percentage, num_combinations=20)
+
+            #print(combinations_question)
+            for comb_question in combinations_question:
+                counter +=1
+                final_question = question
+                final_gt_code = gt_code
+                for value in comb_question.keys():
+                    if value == "{{VALUE_PERCENTAGE}}":
+                       final_question = final_question.replace(value, str(int(comb_question[value]))) 
+                    else: 
+                        final_question = final_question.replace(value, str(int(comb_question[value])+1))
+                    final_gt_code = final_gt_code.replace(value, str(comb_question[value]))
+                    #print("Printing value")
+                    #print(value)
+                    #print("Printing the content of value")
+                    #print(comb_question[value])
+                    #print("printing the question")
+                    #print(final_question)
+                    #print("printing the code")
+                    #print(final_gt_code)
+
+                data_dict[final_question] = {
+                'GT Code': final_gt_code,
+                'GT Objective Value': gt_obj_value,
+                'SGP Code': sgp_code,
+                'SGP Objective Value': sgp_obj_value
+                }
+                
+                # Add the line to the script
+                modified_script =  _insert_code(original_script, final_gt_code)
+                # Write the modified script to a temporary file
+                temp_script_path = '/Users/cchaabani/Documents/GitRepo/SGP-Chat/benchmark/QAs/createbenchmark/temp_scripts_pimp_pexp/temp_script'+str(counter)+'.py'
+                with open(temp_script_path, 'w') as temp_script_file:
+                    temp_script_file.write(modified_script)
+
+                # Execute the modified script
+                execution_rst = _run_with_exec(modified_script)
+                print(execution_rst)
+                data_dict[final_question]['GT Objective Value'] = execution_rst
+                
+    
+    with open(file_path.replace(".csv", "_processed.csv"), 'w', newline='') as csvfile:
+        fieldnames = ['Question', 'GT Code', 'GT Objective Value', 'SGP Code', 'SGP Objective Value']
+        csv_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        # Writing the header
+        csv_writer.writeheader()
+
+        # Writing dictionary entries
+        for question, values in data_dict.items():
+            csv_writer.writerow({'Question': question, **values})
+
+    
+
+
---
+++ benchmark/QAs/createbenchmark/temp_scripts_pimp_pexp/temp_script70.py
@@ -0,0 +1,121 @@
+import gurobipy as gp
+from gurobipy import GRB
+import numpy as np
+
+# Define all parameters
+m = 3  # 5 timesteps
+n = 4  # 6 prosumers
+Pprod = np.array([[65, 45, 36, 25],
+                  [60, 40, 30, 27],
+                  [69, 44, 31, 26]])
+
+Pcons = np.array([[10, 30, 20, 25],
+                  [13, 24, 20, 25],
+                  [25, 26, 20, 25]])
+
+PriceEx = 0.10  # Price of exported power from the paper euro
+PriceImp = 0.13  # Price of imported power from the paper
+
+Dt = 3 * 60  # 300 seconds m*60s, timeslots*60s
+
+SoEmin = [1800] * m  # minimum of SoE (Energy ws)
+SoEmax = [4800] * m  # maximum of SoE  (Energy ws)
+
+Bch_prev = [0, 0, 0, 0]  # Power(w) initialize Battery charge level to zero
+Bdis_prev = [0, 0, 0, 0]  # Power(w) initialize battery discharge level to zero
+
+SoEprev = np.array([2800, 3400, 2500, 3200])  # i(Energy ws) initialize SoE to previous timestep values
+
+capacity = 6000  # maximum capacity of the battery (Energy ws)
+
+Kch = (0.8 * capacity) / Dt  # 40% charging limit Energy/time > Power
+Kdis = (0.8 * capacity) / Dt  # 40% discharging limit Energy/time => Power
+
+# upper and lower bounds
+UB_x = 10000000  # maximum import
+UB_y = 10000000  # maximum export
+
+# Create a model object
+model = gp.Model('model')
+
+# OPTIGUIDE *** CODE GOES HERE
+# OPTIGUIDE DATA CODE GOES HERE
+
+# Create variables
+# decision variables
+Pimp = model.addVars(m, name="Pimp")  # Imported Power
+Pexp = model.addVars(m, name="Pexp")  # Exported Power
+Bch = model.addVars(m, n, name="Bch")  # Charge the battery
+Bdis = model.addVars(m, n, name="Bdis")  # Discharge the battery
+SoE = model.addVars(m, n, name="SoE")  # State of Energy (of the battery)
+b = model.addVars(m, vtype=GRB.BINARY, name="b")  # binary variable
+c = model.addVars(m, n, vtype=GRB.BINARY, name="c")  # binary variable
+SoEnext = model.addVars(m, n, name="SoEnext")  # State of Energy of the battery in the next time timeslot
+
+# Define the objective function
+model.setObjective((PriceImp * Pimp.sum() - PriceEx * Pexp.sum()), GRB.MINIMIZE)
+
+# Constraints
+
+# 1. Energy balance constraint
+for t in range(m):
+    model.addConstr(
+        gp.quicksum(Pprod[t, i] for i in range(n)) - Bch.sum(t, '*') + Bdis.sum(t, '*') + Pimp[t] - Pexp[t] ==
+        np.sum(Pcons[t]))
+
+# 2. No import and export at the same time constraint
+for t in range(m):
+    model.addConstr(Pimp[t] <= (1 - b[t]) * UB_x)
+    model.addConstr(Pexp[t] <= b[t] * UB_y)
+    model.addConstr(Pimp[t] >= 0)
+    model.addConstr(Pexp[t] >= 0)
+
+# ESS constraints, i.e., battery constraints
+model.addConstrs(SoE[0, i] == SoEprev[i] for i in range(n))  # initialize the state of Energy of the battery
+model.addConstrs(SoEnext[t, i] == SoE[t, i] + (Bch[t, i] - Bdis[t, i]) * Dt for t in range(m) for i in range(n))
+model.addConstrs(SoE[t, i] == SoEnext[t - 1, i] for t in range(1, m - 1) for i in range(n))
+model.addConstrs(SoE[m - 1, i] == SoEnext[m - 2, i] for i in range(n))
+
+model.addConstrs(SoEnext[t, i] <= SoEmax[t] for t in range(m) for i in range(n))
+model.addConstrs(SoEnext[t, i] >= SoEmin[t] for t in range(m) for i in range(n))
+
+model.addConstrs(Bch[t, i] >= 0 for t in range(m) for i in range(n))
+model.addConstrs(Bch[t, i] <= Kch for t in range(m) for i in range(n))
+
+model.addConstrs(Bdis[t, i] >= 0 for t in range(m) for i in range(n))
+model.addConstrs(Bdis[t, i] <= Kdis for t in range(m) for i in range(n))
+
+model.addConstrs(Bch[t, i] <= (1 - c[t, i]) * 100000 for t in range(m) for i in range(n))
+model.addConstrs(Bdis[t, i] <= c[t, i] * 100000 for t in range(m) for i in range(n))
+
+model.addConstr(Pimp[0] == Pimp[0-1]*(100+75)/100) 
+model.addConstrs(Pimp[i] >= Pimp[0] for i in range(0+1, len(Pimp)))
+
+
+# Solve the problem using Gurobi solver
+print('SOLVING')
+model.optimize()
+
+'''
+# Get the results
+Pimport = np.array([Pimp[t] for t in range(m)])
+Pexport = np.array([Pexp[t] for t in range(m)])
+Bcharge = np.array([[Bch[t, i] for i in range(n)] for t in range(m)])
+Bdischarge = np.array([[Bdis[t, i]for i in range(n)] for t in range(m)])
+Soe = np.array([[SoE[t, i] for i in range(n)] for t in range(m)])
+soenext = np.array([[SoEnext[t, i] for i in range(n)] for t in range(m)])
+
+# Print results
+print("PV Production", Pprod)
+print("Load", Pcons)
+print("############")
+#print("Objective value:", obj_value)
+print("Import:", Pimport)
+print("Export:", Pexport)
+print("Charge Battery:", Bcharge)
+print("Discharge Battery:", Bdischarge)
+print("SoE:", Soe)
+print("SoEnext:", soenext)
+'''
+
+m = model
